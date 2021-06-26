3ac9f28f2105f07301d7b43dd106b1eef820d15d
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-06-26 21:59:09 +0100

    feat: add routes for registration

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get(
-    '/user', function (Request $request) {
+    '/user',
+    function (Request $request) {
         return $request->user();
     }
 );
+
+Route::post("register", [UserController::class, 'register'])->name('register');
+Route::post("login", [UserController::class, 'login'])->name('login');
