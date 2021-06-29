9e0b60a16c5dc607be5ebce912d8dc45f1f880e9
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-06-29 15:14:37 +0100

    feat: add model, migration table and controllers for videos and comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
 }
--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
-class LoginTest extends TestCase
+class UserLoginTest extends TestCase
 {
     use RefreshDatabase;
 
