e220978b1732b1767bd02e02d139af44bfa60ee8
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-07-06 19:42:54 +0100

    fix: add correct id to index in frontend array

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -52,7 +52,7 @@ class VideoController extends Controller
         $comments = $video->comments()
             ->join('users', 'users.id', 'comments.user_id')
             ->select(
-                'video_id',
+                'id',
                 'users.name',
                 'comment',
             )
